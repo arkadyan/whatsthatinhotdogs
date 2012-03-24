b6ca4afd76bd6c4b590357f4395137f70bbcbc17
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2012-03-24 13:12:18 -0400

    Pull all player data from a local json file.

--- public/javascripts/models.js
+++ public/javascripts/models.js
@@ -7,10 +7,24 @@
   
   window.Players = Backbone.Collection.extend({
     model: Player,
-    url: '/players'
+    url: 'public/players.json',
+    
+    parse: function(response) {
+      var resp = [];
+      var players = response['data']['players'];
+      for (var player in players) {
+        resp.push(players[player]);
+      }
+      return resp;
+    }
+  });
+
+  window.Team = Players.extend({
+    url: 'http://severe-mist-9223.herokuapp.com/team/redsox'
   });
   
   window.players = new Players();
+  window.redsox = new Team();
   
   
   
