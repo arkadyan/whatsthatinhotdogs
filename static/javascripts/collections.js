6a2d7df105aabbfd0149ce822475a84ce40ffc90
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2012-04-25 22:48:14 -0400

    Created a new collections.js file for the collections to separate them out from the models.

---
+++ static/javascripts/collections.js
@@ -0,0 +1,28 @@
+(function($) {
+  
+  window.Players = Backbone.Collection.extend({
+    model: Player,
+    url: '/players',
+    
+    find_by_name: function(name) {
+      return _.find(this.models, function(player) {
+        return player.get("player_full_name") == name
+      });
+    },
+    names: function() {
+      return this.models.map(function(player){
+        return player.get('player_full_name');
+      });
+    }
+  });
+
+
+  window.Team = Players.extend({
+    url: 'http://severe-mist-9223.herokuapp.com/team/redsox'
+  });
+
+
+  window.players = new Players();
+  window.redsox = new Team();
+  
+})(jQuery);
--- static/javascripts/models.js
+++ static/javascripts/models.js
@@ -10,26 +10,6 @@
     }
   });
   
-  window.Players = Backbone.Collection.extend({
-    model: Player,
-    url: '/players',
-    
-    find_by_name: function(name) {
-      return _.find(this.models, function(player) {
-        return player.get("player_full_name") == name
-      });
-    },
-    names: function() {
-      return this.models.map(function(player){
-        return player.get('player_full_name');
-      });
-    }
-  });
-
-  window.Team = Players.extend({
-    url: 'http://severe-mist-9223.herokuapp.com/team/redsox'
-  });
-  
   
   window.Prices = Backbone.Model.extend({
     url: '/prices'
@@ -37,8 +17,5 @@
   
   
   window.prices = new Prices();
-  
-  window.players = new Players();
-  window.redsox = new Team();
   
 })(jQuery);
