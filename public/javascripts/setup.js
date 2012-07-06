45d6073d9828a8926923307a0da2dda2cd046075
the55 <chris@the55.net>
2012-07-06 10:40:46 -0400

    js + asset name refacter / setup

--- public/javascripts/setup.js
+++ public/javascripts/setup.js
@@ -5,15 +5,18 @@ $(function() {
   // Initialize Backbone collections.
   HD.players = new HD.Players();
   HD.redsox = new HD.Team();
-  
+
 
   // Initialize Backbone views.
   HD.playersView = new HD.PlayersView({
     collection: HD.players
   });
-  
+
   HD.router = new HD.Router;
 
   // Initialize the Backbone router.
   Backbone.history.start();
+
+  HD.teams.fetch();
+  HD.players.fetch();
 });
